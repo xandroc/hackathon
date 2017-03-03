397005034a518422b06f0d4e5908b6b53fceaa4f
Josue Balandrano Coronel <xirdneh@gmail.com>
2017-03-03 14:58:08 -0600

    Adding click functino for each marker, clustering and parsing numbers for correct lat and long

--- springs/static/scripts/map.data.js
+++ springs/static/scripts/map.data.js
@@ -12,13 +12,25 @@
     
     function loadMarkers(data){
       var markers = [];
-      _.each(data, function(marker){
+      _.each(data, function(well){
         var m = new google.maps.Marker({
-          position: {lat: Number(marker.LatitudeDD), lng: Number(marker.LongitudeDD)},
+          position: {lat: Number(well.LatitudeDD), lng: Number(well.LongitudeDD)},
           map: window.map._map
+        });
+        m._well = well;
+        m.addListener('click', function(){
+          var s = '<h2>' + well.StateWellNumber + '</h2>' + 
+                  '<ul>' + 
+                    '<li> Well Depth: ' + m._well.WellDepth  + '</li>' + 
+                  '</ul>';        
+          window.map.infoWindow.setContent(s);
+          window.map.infoWindow.open(window.map._map, m);
         });
         markers.push(m);
       });
+      // Add a marker clusterer to manage the markers.
+      var markerCluster = new MarkerClusterer(window.map._map, markers,
+          {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
       window.map.markers = markers;
     }
  
