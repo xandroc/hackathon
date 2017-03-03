ad0164cd002ca00bbda9ff6a21975d27f38053b9
Josue Balandrano Coronel <xirdneh@gmail.com>
2017-03-03 14:28:46 -0600

    Adding google map url

--- springs/urls.py
+++ springs/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^download$', views.download, name='download'),
     url(r'^get_data/(?P<filename>[ \S]+)', views.get_all_json, name='get_all_json'),
+    url(r'^map/', views.render_map, name='render_map')
 ]
